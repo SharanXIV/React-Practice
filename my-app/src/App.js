ee3e886fba8408ec261326557fbce6acdef268d0
Sharan Shanmugaratnam <35677210+SharanXIV@users.noreply.github.com>
2020-03-01 15:35:43 -0500

    Reviewing app basics
    Reviewing react create app function and SPA basics

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Basics Review</h1>
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default App;
