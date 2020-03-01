c50e2f0eabaffae1234309339c9484b1263ef3d7
Sharan Shanmugaratnam <35677210+SharanXIV@users.noreply.github.com>
2020-03-01 16:50:50 -0500

    Lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      {name: 'Sharan',age: 30,belt: 'purple',id: 1},
+      {name: 'Akuma',age: 34,belt: 'black',id: 2},
+      {name: 'Ryu',age: 28,belt: 'red',id: 3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My React Basics Review</h1>
         <p>Welcome!</p>
-        <Ninjas name="Ryu" age="25" belt="Black"/>
-        <Ninjas name="Ken" age="24" belt="Blue"/>
-
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
